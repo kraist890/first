983d38ca6782af59d2076b6aaf9b38324f8479b9
Igor <rr890@yandex.ru>
2020-01-17 22:29:24 +0300

    5 комит

--- js/script.js
+++ js/script.js
@@ -74,3 +74,65 @@ switch (num) {
 		console.log('Что-то пошло не так');
 		break;
 }
+
+// Циклы
+
+while (num < 65) {
+	console.log(num);
+	num++;
+};
+
+for (let i = 1; i < 6; i++) {
+	console.log(i);
+};
+
+for (let i = 1; i < 6; i++) {
+	if (i == 3) {
+		break;
+	};
+	console.log(i);
+}
+
+for (let i = 1; i < 6; i++) {
+	if (i == 3) {
+		continue;
+	};
+	console.log(i);
+};
+
+// Функции
+
+let name = 20;
+function showFirstMessage(text) {
+	alert(text);
+	let name = 10;
+	console.log(name);
+}
+
+showFirstMessage('Hello');
+console.log(name);
+
+/*function calc(a,b) {
+	return (a + b);
+}*/
+
+console.log(3 + 6);
+console.log(8 - 23);
+
+
+let calc = (a,b) => a+b // сокращенная запись вывода функции
+
+console.log(5 + 11);
+
+let str = "Irina";
+console.log(str.length);
+
+console.log(str.toUpperCase()); // буквы верхнего регистра
+console.log(str.toLowerCase()); // буквы нижнего регистра
+
+let crew = "12.3";
+console.log(Math.round(crew)); // приводит к целому числу
+
+let crewToo = "12.3px";
+console.log(parseInt(crewToo)); // приводит к целому числу, убирая всё после точки
+console.log(parseFloat(crewToo)); // оставляет только число
